a68c9784ef2657d7c4033d79326effcddf3cfca8
Afonso Bordado <afonsobordado@az8.co>
2021-12-19 18:45:59 +0000

    Parse GLL data status field

--- src/sentences/gll.rs
+++ src/sentences/gll.rs
@@ -58,11 +58,32 @@ impl From<char> for PosSystemIndicator {
     }
 }
 
+/// This field works with combination with the indicator field.
+///
+/// Quote from the NMEA standard:
+/// "The Status field shall be set to 'V' = Invalid for all values of
+/// Indicator mode except for A = Autonomous and D = Differential."
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum GLLDataStatus {
+    Valid,
+    Invalid,
+}
+
+impl From<char> for GLLDataStatus {
+    fn from(b: char) -> Self {
+        match b {
+            'A' => GLLDataStatus::Valid,
+            _ => GLLDataStatus::Invalid,
+        }
+    }
+}
+
 #[derive(Debug, PartialEq)]
 pub struct GllData {
     pub latitude: f64,
     pub longitude: f64,
     pub fix_time: NaiveTime,
+    pub data_state: GLLDataStatus,
     pub mode: Option<PosSystemIndicator>,
 }
 
@@ -72,7 +93,7 @@ fn do_parse_gll(i: &[u8]) -> IResult<&[u8], GllData> {
     let (i, fix_time) = parse_hms(i)?;
     let (i, _) = take_until(",")(i)?; // decimal ignored
     let (i, _) = char(',')(i)?;
-    let (i, _valid) = char('A')(i)?; // A: valid, V: invalid
+    let (i, data_state) = map(one_of("AV"), GLLDataStatus::from)(i)?; // A: valid, V: invalid
     let (i, _) = char(',')(i)?;
     let (i, mode) = opt(
         map(one_of("ADEM"), PosSystemIndicator::from), // ignore 'N' for invalid
@@ -84,6 +105,7 @@ fn do_parse_gll(i: &[u8]) -> IResult<&[u8], GllData> {
             latitude,
             longitude,
             fix_time,
+            data_state,
             mode,
         },
     ))
@@ -108,6 +130,23 @@ mod tests {
         assert_relative_eq!(gll_data.latitude, 51.0 + (7.0013414 / 60.0));
         assert_relative_eq!(gll_data.longitude, -(114.0 + (2.3279144 / 60.0)));
         assert_eq!(gll_data.fix_time, NaiveTime::from_hms_milli(20, 54, 12, 000));
+        assert_eq!(gll_data.data_state, GLLDataStatus::Valid);
         assert_eq!(gll_data.mode, Some(PosSystemIndicator::Autonomous));
+    }
+
+    #[test]
+    fn test_parse_void_gngll() {
+        let s = parse_nmea_sentence(
+            b"$GNGLL,5107.0014143,N,11402.3278489,W,205122.00,V,E*7D"
+        ).unwrap();
+        assert_eq!(s.checksum, s.calc_checksum());
+        assert_eq!(s.checksum, 0x7D);
+
+        let gll_data = parse_gll(s).unwrap();
+        assert_relative_eq!(gll_data.latitude, 51.0 + (7.0014143 / 60.0));
+        assert_relative_eq!(gll_data.longitude, -(114.0 + (2.3278489 / 60.0)));
+        assert_eq!(gll_data.fix_time, NaiveTime::from_hms_milli(20, 51, 22, 000));
+        assert_eq!(gll_data.data_state, GLLDataStatus::Invalid);
+        assert_eq!(gll_data.mode, Some(PosSystemIndicator::EstimatedMode));
     }
 }
